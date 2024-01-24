59d9a7812aec269b6b813bc2e5d1a9f1a9a0c9f0
Andrei Lefter <92049917+andlft@users.noreply.github.com>
2024-01-24 15:36:41 +0200

    Improve UI

--- Proiect/Assets/Scripts/LevelSelector.cs
+++ Proiect/Assets/Scripts/LevelSelector.cs
@@ -1,17 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
-    public Button myButton;
+    [SerializeField] int SceneID;
 
     public void LoadLevel()
     {
-        Text buttonText = myButton.GetComponentInChildren<Text>();
-        string textValue = buttonText.text;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + ((int)textValue[6]) - 48);
+        SceneManager.LoadScene(SceneID);
     }
 }
